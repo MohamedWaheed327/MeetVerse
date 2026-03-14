aeafdc6baaf57f18f69a6a9f3e0e5a901e0c84cc
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-14 10:38:25 +0200

    add MeetingDtos and another .gitignore

---
+++ MeetVerseBackend/MeetVerse.Api/Dtos/MeetingsDtos.cs
@@ -0,0 +1,11 @@
+
+namespace MeetVerse.Api.Dtos;
+
+class MeetingsRequest
+{
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public DateTime ScheduledStart { get; set; }
+    public DateTime? ScheduledEnd { get; set; }
+    public Guid? GroupId { get; set; } = new Guid("75E7F53E-5540-4747-A02A-B877C2FDC2F5");
+}
