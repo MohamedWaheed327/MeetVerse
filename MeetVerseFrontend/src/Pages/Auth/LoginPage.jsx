026b60b1e518b6a877d16d8a20233a2f9694b6db
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-21 13:50:12 +0200

    bug fix

--- MeetVerseFrontend/src/Pages/Auth/LoginPage.jsx
+++ MeetVerseFrontend/src/Pages/Auth/LoginPage.jsx
@@ -19,7 +19,6 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      alert("hi");
       const data = { email, password };
       const response = await loginUser(data);
       // save token and redirect
