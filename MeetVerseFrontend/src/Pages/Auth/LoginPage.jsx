716a8c210140e46b5cc7a5489f2bfcad09acf5cb
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-21 13:47:05 +0200

    debug

--- MeetVerseFrontend/src/Pages/Auth/LoginPage.jsx
+++ MeetVerseFrontend/src/Pages/Auth/LoginPage.jsx
@@ -19,6 +19,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
+      alert("hi");
       const data = { email, password };
       const response = await loginUser(data);
       // save token and redirect
--- MeetVerseFrontend/src/services/login.js
+++ MeetVerseFrontend/src/services/login.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const loginUser = async (data) => {
     try {
-        alert(data);
+        alert("hi2");
         const response = await api.post("/auth/login", data);
         return response.data;
     } catch (error) {
