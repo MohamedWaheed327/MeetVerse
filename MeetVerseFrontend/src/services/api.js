b9386af4e6cafa0dbc87d94b7246e7978adb75d0
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-21 10:05:37 +0200

    correct backend host

--- MeetVerseFrontend/src/services/api.js
+++ MeetVerseFrontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // TODO: put host in json file
 // const host = "http://meetverse-env.eba-qcpudpcc.us-east-1.elasticbeanstalk.com"; // include protocol to avoid relative path issues
-const host = "https://d278p5zvdcqqh2.cloudfront.net/"; // include protocol to avoid relative path issues
+const host = "https://d278p5zvdcqqh2.cloudfront.net"; // include protocol to avoid relative path issues
 
 const api = axios.create({
     baseURL: host + "/api",
--- MeetVerseFrontend/src/services/hubs/connections.js
+++ MeetVerseFrontend/src/services/hubs/connections.js
@@ -2,7 +2,7 @@ import * as signalR from "@microsoft/signalr";
 
 // TODO: put host in json file
 // const host = "http://meetverse-env.eba-qcpudpcc.us-east-1.elasticbeanstalk.com"; // include protocol to avoid relative path issues
-const host = "https://d278p5zvdcqqh2.cloudfront.net/"; // include protocol to avoid relative path issues
+const host = "https://d278p5zvdcqqh2.cloudfront.net"; // include protocol to avoid relative path issues
 
 const connection = new signalR.HubConnectionBuilder()
     .withUrl(host + "/hubs/meetingchat", {
