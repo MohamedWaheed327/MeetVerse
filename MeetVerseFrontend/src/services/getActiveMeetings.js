a5709a1b7d071f308044f841c798db192a269c4a
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-24 06:50:20 +0200

    get active meetings api

---
+++ MeetVerseFrontend/src/services/getActiveMeetings.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export const getActiveMeetings = async () => {
+    try {
+        const response = await api.get("/meetings/active-meetings");
+        const now = new Date(); // Current time
+        const processedData = response.data.map(meeting => ({
+            ...meeting,
+            isLive: new Date(meeting.scheduledStart) <= now && new Date(meeting.scheduledEnd) >= now,
+            meetingId: meeting.id            
+        }));
+
+        return processedData;
+    } catch (error) {
+        if (error.response) {
+            throw error.response.data;
+        } else {
+            throw { message: "Network error" };
+        }
+    }
+};
