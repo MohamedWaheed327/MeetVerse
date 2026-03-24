ec0d4e13f34562fb23733d56e039cbfa30679e01
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2026-03-24 17:19:56 +0200

    commented contriant

--- MeetVerseBackend/MeetVerse.Api/Data/Configurations/MeetingParticipantConfiguration.cs
+++ MeetVerseBackend/MeetVerse.Api/Data/Configurations/MeetingParticipantConfiguration.cs
@@ -4,11 +4,14 @@ using MeetVerse.Api.Models;
 
 namespace MeetVerse.Api.Data.Configurations;
 
+// TODO: add the commented constraint
 public class MeetingParticipantConfiguration : IEntityTypeConfiguration<MeetingParticipant>
 {
     public void Configure(EntityTypeBuilder<MeetingParticipant> builder)
     {
         builder.Property(mp => mp.Role)
             .HasConversion<string>();
+        // builder.HasIndex(p => new { p.UserId, p.MeetingId })
+        //     .IsUnique();
     }
 }
