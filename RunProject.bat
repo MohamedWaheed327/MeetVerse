0099b25396b14e6a341a9885163c0c786b28b52b
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-04-25 10:21:08 +0300

    update RunProject.bat

--- RunProject.bat
+++ RunProject.bat
@@ -1,20 +1,16 @@
 @echo off
 setlocal
 
-REM Kill anything using frontend/backend ports
 for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5173 ^| findstr LISTENING') do taskkill /F /PID %%a >nul 2>&1
 for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5279 ^| findstr LISTENING') do taskkill /F /PID %%a >nul 2>&1
 
-REM Start backend minimized
-start "MeetVerse Backend" /min cmd /c "cd /d MeetVerseBackend\MeetVerse.Api && dotnet restore && dotnet build && dotnet run"
+cd /d MeetVerseBackend\MeetVerse.Api
+start /B cmd /c "dotnet restore && dotnet build && dotnet run"
 
-REM Start frontend minimized
-start "MeetVerse Frontend" /min cmd /c "cd /d MeetVerseFrontend && npm run dev"
+cd /d ..\..\MeetVerseFrontend
+start /B cmd /c "npm run dev"
 
-REM Wait a bit for frontend to boot
-timeout /t 6 >nul
-
-REM Open browser
+REM timeout /t 6 >nul
 start "" "http://localhost:5173"
 
 endlocal
