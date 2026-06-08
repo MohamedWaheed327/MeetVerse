1fcb2791bd1e8847b1344fa3366d4150598ffea1
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-06-08 14:17:10 +0300

    change connection string and backend url

--- MeetVerseBackend/MeetVerse.Api/Database/MeetVerseDbContextFactory.cs
+++ MeetVerseBackend/MeetVerse.Api/Database/MeetVerseDbContextFactory.cs
@@ -11,7 +11,7 @@ public class MeetVerseDbContextFactory : IDesignTimeDbContextFactory<MeetVerseDb
 
         // This connection string is strictly for EF Core design-time tools (migrations).
         // It does not override appsettings.json at runtime.
-        var connectionString = "";
+        var connectionString = "Server=meetverse-database.csr4uc44qxg8.us-east-1.rds.amazonaws.com,1433;Database=MeetVerseDbFactory;User Id=admin;Password=pWKX8mOOt5cjTHpH3OmW;Encrypt=True;TrustServerCertificate=True;";
 
         optionsBuilder.UseSqlServer(connectionString);
 
