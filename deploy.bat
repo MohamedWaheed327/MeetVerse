caddd55c64e5a6e4d42bc92ec40b0f8150c09918
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-06-09 06:59:14 +0300

    add deployment script

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+call d:/vscode/python-environments/ebcli-env/scripts/activate
+cd MeetVerseBackend/MeetVerse.Api
+dotnet build && dotnet publish -o site && cd site
+eb init MeetVerseBackend --region us-east-1 -p 64bit-amazon-linux-2023-v3.11.1-running-.net-10
+REM eb terminate MeetVerseEnv
+REM eb create MeetVerseEnv
+eb deploy MeetVerseEnv
+eb open MeetVerseEnv
+cd ..
+rmdir /s /q "site"
+call d:/vscode/python-environments/ebcli-env/scripts/deactivate
