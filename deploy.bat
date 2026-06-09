2cf17001651fc09536d010df3f01f6603d640f3c
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-06-09 15:15:04 +0300

    modify .gitignore && depoly.bat

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 call d:/vscode/python-environments/ebcli-env/scripts/activate
-cd MeetVerseBackend/MeetVerse.Api
+cd MeetVerseBackend/MeetVerse.Web
 dotnet build && dotnet publish -o site && cd site
 eb init MeetVerseBackend --region us-east-1 -p 64bit-amazon-linux-2023-v3.11.1-running-.net-10
 REM eb terminate MeetVerseEnv
