6c07e07df94464e307edda871115404954cb1d3a
MohamedWaheed327 <mohamed.waheed.327.0@gmail.com>
2026-06-13 00:08:49 +0300

    feat: enhance audio processing by adding input chunk handling and optimizing inference flow

--- fastapi-server/main.py
+++ fastapi-server/main.py
@@ -40,12 +40,13 @@ class SessionState:
     def __init__(self):
         self.h = None
 
+        self.queue = np.zeros(2912 * 2, dtype=np.float32)
+
         # raw audio received from client
         self.input_chunks = []
 
         # enhanced audio returned by model
         self.output_chunks = []
-
 
 sessions = {}
 
@@ -55,22 +56,16 @@ sessions = {}
 def decode_audio(bytes_data):
     return np.frombuffer(bytes_data, dtype=np.float32)
 
-
 def encode_audio(wav: np.ndarray):
     return wav.astype(np.float32).tobytes()
 
-
 def save_session_audio(user_id: str, state: SessionState):
     try:
-
         # ----------------------------
         # SAVE INPUT
         # ----------------------------
         if state.input_chunks:
-
-            input_audio = np.concatenate(
-                state.input_chunks
-            )
+            input_audio = np.concatenate(state.input_chunks)
 
             input_file = os.path.join(
                 RECORDINGS_DIR,
@@ -116,56 +111,28 @@ def save_session_audio(user_id: str, state: SessionState):
             f"Failed saving audio for {user_id}: {e}"
         )
 
-
 # ----------------------------
 # FAST SAFE INFERENCE WRAPPER
 # ----------------------------
-async def run_inference(
-    wav_tensor,
-    state: SessionState
-):
-
+async def run_inference(wav_tensor,state: SessionState):
     def _infer():
-
         with torch.inference_mode():
+            noisy_spec = transform.audio_to_spectrogram(wav_tensor) # STFT
 
-            noisy_spec = (
-                transform.audio_to_spectrogram(
-                    wav_tensor
-                )
-            )
+            mask, new_h = model(noisy_spec, state.h)
 
-            mask, new_h = model(
-                noisy_spec,
-                state.h
-            )
+            enhanced_spec = transform.apply_mask(noisy_spec, mask)
 
-            enhanced_spec = (
-                transform.apply_mask(
-                    noisy_spec,
-                    mask
-                )
-            )
+            # enhanced_spec = noisy_spec
+            # new_h = state.h
 
-            enhanced_wav = (
-                transform.spectrogram_to_audio(
-                    enhanced_spec,
-                    target_num_samples=wav_tensor.shape[-1]
-                )
-            )
+            enhanced_wav = transform.spectrogram_to_audio(enhanced_spec, target_num_samples=wav_tensor.shape[-1]) # ISTFT
 
             return enhanced_wav, new_h
 
-    enhanced_wav, new_h = (
-        await asyncio.to_thread(_infer)
-    )
+    enhanced_wav, new_h = await asyncio.to_thread(_infer)
 
-    state.h = (
-        new_h.detach()
-        if new_h is not None
-        else None
-    )
-
+    state.h = new_h.detach() if new_h is not None else None
     return enhanced_wav, state.h
 
 
@@ -173,40 +140,30 @@ async def run_inference(
 # WEBSOCKET ENDPOINT
 # ----------------------------
 @app.websocket("/ws/audio")
-async def audio_ws(
-    websocket: WebSocket
-):
-
+async def audio_ws(websocket: WebSocket):
     await websocket.accept()
 
     user_id = str(uuid.uuid4())
-
     sessions[user_id] = SessionState()
     state = sessions[user_id]
 
-    print(
-        f"Client connected: {user_id}"
-    )
+    print(f"Client connected: {user_id}" )
 
     try:
-
         while True:
-
             # ----------------------------
             # RECEIVE AUDIO
             # ----------------------------
-            audio_bytes = (
-                await websocket.receive_bytes()
-            )
+            audio_bytes = await websocket.receive_bytes()
 
-            wav = decode_audio(
-                audio_bytes
-            )
+            wav = decode_audio(audio_bytes) # 160 sample
+            
+            # print(len(audio_bytes), wav.shape)
+            # state.queue = np.concatenate([state.queue[len(wav):], wav])
+            # wav = np.asarray(state.queue, dtype=np.float32)
 
             # Save incoming audio
-            state.input_chunks.append(
-                wav.copy()
-            )
+            state.input_chunks.append(wav.copy())
 
             wav = torch.tensor(
                 wav,
@@ -217,99 +174,39 @@ async def audio_ws(
             # ----------------------------
             # INFERENCE
             # ----------------------------
-            enhanced_wav, new_h = (
-                await run_inference(
-                    wav,
-                    state
-                )
-            )
-
+            enhanced_wav, new_h = (await run_inference(wav,state))
             state.h = new_h
 
             # ----------------------------
             # CONVERT OUTPUT
             # ----------------------------
-            output = (
-                enhanced_wav
-                .squeeze(0)
-                .detach()
-                .cpu()
-                .numpy()
-            )
+            output = enhanced_wav.squeeze(0).detach().cpu().numpy()
+            # output = output[-160:]
 
             # Save enhanced audio
-            state.output_chunks.append(
-                output.copy()
-            )
-
-            # Debug difference
-            input_np = (
-                wav
-                .squeeze(0)
-                .detach()
-                .cpu()
-                .numpy()
-            )
-
-            diff = np.mean(
-                np.abs(
-                    input_np - output
-                )
-            )
-
-            print(
-                f"[DIFF L1] {diff:.6f}"
-            )
+            state.output_chunks.append(output.copy())
 
             # ----------------------------
             # SEND RESULT
             # ----------------------------
-            await websocket.send_bytes(
-                encode_audio(output)
-            )
+            await websocket.send_bytes(encode_audio(output))
 
     except WebSocketDisconnect:
-
-        print(
-            f"Disconnected: {user_id}"
-        )
-
-        save_session_audio(
-            user_id,
-            state
-        )
-
-        sessions.pop(
-            user_id,
-            None
-        )
+        print(f"Disconnected: {user_id}")
+        save_session_audio(user_id, state)
+        sessions.pop(user_id, None)
 
     except Exception as e:
-
-        print(
-            f"Error: {e}"
-        )
-
-        save_session_audio(
-            user_id,
-            state
-        )
-
-        sessions.pop(
-            user_id,
-            None
-        )
-
+        print(f"Error: {e}")
+        save_session_audio(user_id, state)
+        sessions.pop(user_id, None)
 
 # ----------------------------
 # HEALTH CHECK
 # ----------------------------
 @app.get("/")
 def root():
-    return {
-        "status": "running"
-    }
-
+    return {"status": "running"}
 
 # Run:
 # uvicorn main:app --host 0.0.0.0 --port 8000
