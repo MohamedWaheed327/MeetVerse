a87b2c69931a483e9d2e81e358e416ac32542568
ahmedrezk14 <114368625+ahmedrezk14@users.noreply.github.com>
2026-06-17 04:37:13 +0300

    Ahmed Rizk
    Refactor inference model by removing unused code and placeholders.

--- Noise Cancelation Model/inference_model.py
+++ Noise Cancelation Model/inference_model.py
@@ -17,165 +17,19 @@ REAL_TIME_CHUNK_FRAMES=4
 
 
 def make_group_norm(channels,max_groups=8):
-    groups=min(channels,max_groups)
-    while groups>1 and channels%groups!=0:
-        groups-=1
-    return nn.GroupNorm(groups,channels)
+    pass
 
 class SEBlock(nn.Module): # attention layer
-    def __init__(self,channels,reduction=8):
-        super().__init__()
-        self.pool=nn.AdaptiveAvgPool2d(1)
-        hidden=max(channels//reduction,4)
-        self.fc=nn.Sequential(
-            nn.Conv2d(channels,hidden,kernel_size=1,bias=False),
-            nn.SiLU(inplace=True),
-            nn.Conv2d(hidden,channels,kernel_size=1,bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self,x):
-        w=self.pool(x)
-        w=self.fc(w)
-        return w*x
+    pass
 
 class ResSEBlock(nn.Module):
-    def __init__(self,in_c,out_c,stride):
-        super().__init__()
-        self.act=nn.SiLU(inplace=True)
-
-        self.conv1=nn.Conv2d(in_c,out_c,kernel_size=3,stride=stride,padding=1)
-        self.gn1=make_group_norm(out_c) # best for realtime
-
-        self.conv2=nn.Conv2d(out_c,out_c,kernel_size=3,padding=1)
-        self.gn2=make_group_norm(out_c)
-
-        self.se=SEBlock(out_c)
-
-        if stride!=1 or in_c!=out_c:
-            self.shortcut=nn.Sequential(
-                nn.Conv2d(in_c,out_c,kernel_size=1,stride=stride),
-                make_group_norm(out_c)
-            )
-        else :
-            self.shortcut=nn.Identity()
-
-    def forward(self,x):
-        identity=self.shortcut(x)
-
-        out=self.conv1(x)
-        out=self.gn1(out)
-        out=self.act(out)
-
-        out=self.conv2(out)
-        out=self.gn2(out)
-
-        out=self.se(out)
-
-        out=out+identity
-        out=self.act(out)
-
-        return out
+    pass
 
 def up_Block(in_c,out_c):
-    return nn.Sequential(
-        nn.Upsample(scale_factor=(2,1),mode='bilinear',align_corners=False),
-        nn.Conv2d(in_c,out_c,kernel_size=3,padding=1),
-        make_group_norm(out_c),
-        nn.SiLU(inplace=True)
-    )
+    pass
 
 class Model (nn.Module):
-    def __init__(self, base=48,gru_hidden_sz=256,layers=1,freq_bins=256):
-        super().__init__()
-        self.base=base
-        self.gru_hidden_sz=gru_hidden_sz
-        self.layers=layers
-        self.freq_bins=freq_bins
-
-        # Encoding
-        self.enc1=ResSEBlock(2,self.base,stride=(2,1))
-        self.enc2=ResSEBlock(self.base,self.base*2,stride=(2,1))
-        self.enc3=ResSEBlock(self.base*2,self.base*4,stride=(2,1))
-        self.enc4=ResSEBlock(self.base*4,self.base*8,stride=(2,1))
-
-
-        # GRU
-
-        self.bottleneck_freq_bins=self.freq_bins//16
-        self.gru_in_sz=(self.base*8)*self.bottleneck_freq_bins
-
-
-        self.gru=nn.GRU(
-            input_size=self.gru_in_sz,
-            hidden_size=self.gru_hidden_sz,
-            num_layers=self.layers,
-            batch_first=True,
-            bidirectional=False
-        )
-
-        self.gru_fc=nn.Sequential(
-            nn.Linear(self.gru_hidden_sz,self.gru_in_sz),
-            nn.SiLU(inplace=True),
-        )
-
-        # DECODE
-        self.dec4=up_Block(self.base*16,self.base*4)
-        self.dec3=up_Block(self.base*8,self.base*2)
-        self.dec2=up_Block(self.base*4,self.base)
-
-        # self.mask_conv=nn.Sequential()-----------
-        self.mask_conv=nn.Sequential(
-            nn.Upsample(scale_factor=(2,1),mode='bilinear',align_corners=False),
-            nn.Conv2d(self.base*2,2,kernel_size=3,padding=1)
-        )
-
-        self.sig=nn.Sigmoid()
-
-    @staticmethod
-    def match_shape(x,target):
-        diff_f=target.size(2)-x.size(2)
-        diff_t=target.size(3)-x.size(3)
-        if diff_f==0 and diff_t==0:
-            return x
-        return F.pad(x,(diff_t//2,diff_t-diff_t//2,diff_f//2,diff_f-diff_f//2,))
-
-
-    def forward(self,x,h=None):
-        e1=self.enc1(x)
-        e2=self.enc2(e1)
-        e3=self.enc3(e2)
-        e4=self.enc4(e3)
-
-        b,c,f,t=e4.shape
-
-        # seq -------------------
-        seq=e4.permute(0,3,1,2).reshape(b,t,c*f)
-
-        if c*f!=self.gru_in_sz:
-            raise ValueError(f"GRU input mismatch: got {c*f}, expected {self.gru_in_sz}")
-
-        gru_out,next_h=self.gru(seq,h)
-        gru_out=self.gru_fc(gru_out)
-        gru_out=gru_out.reshape(b,t,c,f).permute(0,2,3,1)
-
-
-        d4=self.dec4(torch.cat([gru_out,e4],dim=1))
-        d4=self.match_shape(d4,e3)
-
-        d3=self.dec3(torch.cat([d4,e3],dim=1))
-        d3=self.match_shape(d3,e2)
-
-        d2=self.dec2(torch.cat([d3,e2],dim=1))
-        d2=self.match_shape(d2,e1)
-
-
-        mask=self.mask_conv(torch.cat([d2,e1],dim=1))
-        mask=self.match_shape(mask,x)
-
-        mask=self.sig(mask)
-
-        return mask,next_h
+    pass
 
 class SpectrogramTransform:
     def __init__(self,n_fft=N_FFT,hop_length=HOP_LENGTH,center=CENTER,device='cpu'):
@@ -243,12 +97,7 @@ class SpectrogramTransform:
         return wav
 
 def load_model(device):
-    model = Model(
-        base=48,
-        gru_hidden_sz=512,
-        layers=1,
-        freq_bins=256
-    ).to(device)
+    model = Model().to(device)
 
     state_dict = torch.load(
         "model_only.pth",  
@@ -259,35 +108,3 @@ def load_model(device):
     model.eval()
 
     return model
-
-def split_spec_chunks(spec,chunk_frames=TRAIN_CHUNK_FRAMES):
-    chunks=[]
-    original_T=spec.size(-1)
-    for start in range(0,original_T,chunk_frames):
-        chunk=spec[:,:,:,start:start+chunk_frames]
-        if chunk.size(-1)<chunk_frames:
-            pad_t=chunk_frames-chunk.size(-1)
-            chunk=F.pad(chunk,(0,pad_t))
-        chunks.append(chunk)
-    return chunks,original_T
-
-def enhance_waveform_streaming(noisy_wav,model,transform,chunk_frames=TRAIN_CHUNK_FRAMES,h=None,detach_every=DETACH_EVERY):
-    noisy_spec=transform.audio_to_spectrogram(noisy_wav)
-    chunks,original_T=split_spec_chunks(noisy_spec,chunk_frames=chunk_frames)
-    enhanced_chunks=[]
-    next_h=h
-
-    for i,spec_chunk in enumerate(chunks):
-        mask,next_h=model(spec_chunk,next_h)
-        enhanced_chunk=transform.apply_mask(spec_chunk,mask)
-        enhanced_chunks.append(enhanced_chunk)
-
-        if detach_every is not None and (i+1)%detach_every==0:
-            if next_h is not None:
-                next_h=next_h.detach()
-    enhanced_spec=torch.cat(enhanced_chunks,dim=-1)
-    enhanced_spec=enhanced_spec[:,:,:,:original_T]
-
-    enhanced_wav=transform.spectrogram_to_audio(enhanced_spec,target_num_samples=noisy_wav.shape[-1])
-
-    return enhanced_wav,enhanced_spec,next_h
